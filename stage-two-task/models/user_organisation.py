6c8c1ef9d5edf279d354215aecaa1199a2134aa4
johnson-oragui <johnson.oragui@gmail.com>
2024-07-05 15:15:36 +0100

    added a primary and String data type

--- stage-two-task/models/user_organisation.py
+++ stage-two-task/models/user_organisation.py
@@ -2,7 +2,7 @@
 """
 User_Organisation association model
 """
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, String, Integer
 from sqlalchemy.orm import Mapped, mapped_column
 from models.base import Base
 
@@ -11,14 +11,17 @@ class User_Organisation(Base):
     Class Organisation that acts as the representation for organisation tables in the
     Database
     """
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     # userId represents the id of the user associated with the organisation
     userId: Mapped[str] = mapped_column(
+        String(60),
         ForeignKey("users.userId"),
         primary_key=True,
         nullable=False
         )
     # orgId represents the id of the organisation associated with the user
     orgId: Mapped[str] = mapped_column(
+        String(60),
         ForeignKey("organisations.orgId"),
         primary_key=True,
         nullable=False
