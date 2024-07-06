8c2c9a834f75c35cd70fcc03dded5cda8f612984
johnson-oragui <johnson.oragui@gmail.com>
2024-07-06 16:36:55 +0100

    register organisation blueprint

--- stage-two-task/api/app/routes/__init__.py
+++ stage-two-task/api/app/routes/__init__.py
@@ -7,6 +7,8 @@ Blueprints to be registered
 def register_routes(app):
     from api.app.routes.auth import auth
     from api.app.routes.dashboard import dashboard
+    from api.app.routes.organisation import organisation
 
     app.register_blueprint(auth)
     app.register_blueprint(dashboard)
+    app.register_blueprint(organisation)
