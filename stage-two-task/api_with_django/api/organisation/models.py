db9ea18c83b79184f98e8790605a44fedc9d71a5
johnson-oragui <johnson.oragui@gmail.com>
2024-07-09 10:19:47 +0100

    added organisation model

---
+++ stage-two-task/api_with_django/api/organisation/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.utils.timezone import now
+from uuid import uuid4
+
+
+class Organisation(models.Model):
+    id = models.CharField(primary_key=True, unique=True, null=False, default=str(uuid4()), auto_created=True, max_length=60)
+    name = models.CharField(null=False, max_length=60)
+    description = models.TextField(max_length=1000)
+    owner_email = models.CharField(null=False, blank=False, max_length=60)
+    create_at = models.DateField(default=now)
+    update_at = models.DateField(default=now)
+
+    class Meta:
+        db_table = "organisations"
