912dd18684e386ca8f7f21cfecf5b2a4147bb3ce
johnson-oragui <johnson.oragui@gmail.com>
2024-07-09 13:33:04 +0100

    updated field attributes

--- stage-two-task/api_with_django/api/organisation/models.py
+++ stage-two-task/api_with_django/api/organisation/models.py
@@ -5,9 +5,9 @@ from uuid import uuid4
 
 class Organisation(models.Model):
     id = models.CharField(primary_key=True, unique=True, null=False, default=str(uuid4()), auto_created=True, max_length=60)
-    name = models.CharField(null=False, max_length=60)
+    name = models.CharField(null=False, max_length=100)
     description = models.TextField(max_length=1000)
-    owner_email = models.CharField(null=False, blank=False, max_length=60)
+    owner_email = models.EmailField(null=False, blank=False, max_length=60)
     create_at = models.DateField(default=now)
     update_at = models.DateField(default=now)
 
