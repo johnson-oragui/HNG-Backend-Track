66b15b7e1405fc8bcec64a22aa6486e1ff9c15ba
johnson-oragui <johnson.oragui@gmail.com>
2024-07-09 15:21:37 +0100

    used the gen_uuid_str function to create id field

--- stage-two-task/api_with_django/api/organisation/models.py
+++ stage-two-task/api_with_django/api/organisation/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 from django.utils.timezone import now
-from uuid import uuid4
+from api.utils import gen_uuid_str
 
 
 class Organisation(models.Model):
-    id = models.CharField(primary_key=True, unique=True, null=False, default=str(uuid4()), auto_created=True, max_length=60)
+    id = models.CharField(primary_key=True, unique=True, null=False, default=gen_uuid_str, auto_created=True, max_length=60)
     name = models.CharField(null=False, max_length=100)
     description = models.TextField(max_length=1000)
     owner_email = models.EmailField(null=False, blank=False, max_length=60)
