1821f85e089a612c092a894c210979efcaddc607
johnson-oragui <johnson.oragui@gmail.com>
2024-07-09 13:28:57 +0100

    removed the actions before user creation

--- stage-two-task/api_with_django/api/api/models.py
+++ stage-two-task/api_with_django/api/api/models.py
@@ -16,20 +16,21 @@ class MyUserModelManager(models.Manager):
 
         # Create the user instance
         new_user = super().create(**kwargs)  # here, the object is created
-        description = f"{new_user.first_name} is the Greatest"
+        # org_name = f"{new_user.firstName}'s Organisaton"
+        # description = f"{new_user.first_name}'s Organisation is the Greatest"
 
-        # Create the organisation with user's firstName
-        organisation = Organisation.objects.create(name=new_user.first_name,
-                                                   owner_email=new_user.email,
-                                                   description=description)
+        # # Create the organisation with user's firstName
+        # organisation = Organisation.objects.create(name=org_name,
+        #                                            owner_email=new_user.email,
+        #                                            description=description)
 
-        # Assuming kwargs includes a "role" parameter
-        role = kwargs.get('role', 'owner')  # Default to "owner" if not provided
+        # # Assuming kwargs includes a "role" parameter
+        # role = kwargs.get('role', 'owner')  # Default to "owner" if not provided
 
-        # Create user-organisation association
-        user_org = UserOrganisation.objects.create(user=new_user,
-                                                   organisation=organisation,
-                                                   role=role)
+        # # Create user-organisation association
+        # user_org = UserOrganisation.objects.create(user=new_user,
+        #                                            organisation=organisation,
+        #                                            role=role)
 
         return new_user  # return instance
 
@@ -52,44 +53,3 @@ class MyUserModelManager(models.Manager):
             raise Exception(
                 "Could not hash password, check problems!."
             ) from exc
-
-    def get_user_by_email_or_id(self, email=None, user_id=None):
-        """
-        Retrieve user with email or user_id
-        """
-        try:
-            if email and not user_id:
-                user = self.filter(email=email)
-                if user:
-                    return user
-                else:
-                    return
-            if user_id and not email:
-                user = self.filter(user_id=user_id)
-                if user:
-                    return user
-                else:
-                    return
-            if user_id and email:
-                user = self.filter(user_id=user_id, email=email)
-                if user:
-                    return user
-                else:
-                    return
-        except Exception as exc:
-            print(f'error retrieving user by email or id: {exc}')
-            return
-
-    def update_user(self, data: dict = None):
-        """
-        Update a user
-        """
-        if not data or not isinstance(data, dict):
-            raise ValueError(f"{data} is empty or not a dictionary")
-
-        try:
-            updated_usser = self.update(**data)
-            return updated_usser
-        except Exception as exc:
-            print(f'error updating user: {exc}')
-            raise Exception("could not update user") from exc
