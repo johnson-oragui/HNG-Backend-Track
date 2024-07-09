5c9aa8764c6cb8abb090b7ec6f6febad0874bfd3
johnson-oragui <johnson.oragui@gmail.com>
2024-07-09 13:37:11 +0100

    added pre_delete signal to only delete if user role is the owner, and pre_save for updated_at field

---
+++ stage-two-task/api_with_django/api/user_organisation/signals.py
@@ -0,0 +1,48 @@
+from django.db.models.signals import pre_delete, pre_save
+from django.utils.timezone import now
+from django.dispatch import receiver
+from django.core.exceptions import PermissionDenied
+from user_organisation.models import UserOrganisation
+
+
+@receiver(pre_delete, sender=UserOrganisation)
+def confirm_owner_before_delete(sender, instance, using, origin, **kwargs):
+    """
+    Confirms that the row been deleted is by the owner
+
+    Params:
+        sender: The model instance that has been created and saved
+        instance: The model object that was created
+        using: The database alias being used.
+        origin: The origin of the deletion being the instance of a Model or QuerySet class.
+    """
+    try:
+        if not instance.role == 'owner':
+            raise PermissionDenied(
+            f"Could not delete user_organisation, must be the owner to delete!: {instance.owner}"
+            ) from PermissionDenied
+        else:
+            print('User organisation deletion initiated by owner...')
+    except Exception as exc:
+        print(f'error deleting user_organisation: {exc}')
+        raise Exception(
+            "Could not delete user_organisation"
+            ) from exc
+    finally:
+        print('end of pre_delete signal for user_organisation deletion!')
+
+@receiver(pre_save, sender=UserOrganisation)
+def update_updated_at_field(sender, instance, **kwargs):
+    """
+
+    """
+    try:
+        if instance.pk:
+            instance.updated_at = now()
+    except Exception as exc:
+        print(f'error updating user_organisation: {exc}')
+        raise Exception(
+            "Could not updating user_organisation"
+            ) from exc
+    finally:
+        print('end of pre_save signal for user_organisation update!')
