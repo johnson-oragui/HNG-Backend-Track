1514f9f7737f3484077e39dd09fa930c503a67f1
johnson-oragui <johnson.oragui@gmail.com>
2024-07-09 13:35:12 +0100

    imported signals

--- stage-two-task/api_with_django/api/organisation/apps.py
+++ stage-two-task/api_with_django/api/organisation/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class OrganisationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'organisation'
+
+    def ready(self):
+        """
+        Import signals
+        """
+        from organisation import signals
