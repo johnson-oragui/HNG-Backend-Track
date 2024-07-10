0610ab2af8a3f7f80cc4464f578482f81fc6b731
johnson-oragui <johnson.oragui@gmail.com>
2024-07-10 09:31:06 +0100

    decoded hashed password, removed commented logics

--- stage-two-task/api_with_django/api/api/models.py
+++ stage-two-task/api_with_django/api/api/models.py
@@ -7,8 +7,6 @@ class MyUserModelManager(models.Manager):
         """
         Intercept user creation to has password
         """
-        from organisation.models import Organisation
-        from user_organisation.models import UserOrganisation
         plain_passwd = kwargs.get('password')
         if plain_passwd:
             hashed_pwd = MyUserModelManager.hash_password(plain_passwd)
@@ -16,21 +14,6 @@ class MyUserModelManager(models.Manager):
 
         # Create the user instance
         new_user = super().create(**kwargs)  # here, the object is created
-        # org_name = f"{new_user.firstName}'s Organisaton"
-        # description = f"{new_user.first_name}'s Organisation is the Greatest"
-
-        # # Create the organisation with user's firstName
-        # organisation = Organisation.objects.create(name=org_name,
-        #                                            owner_email=new_user.email,
-        #                                            description=description)
-
-        # # Assuming kwargs includes a "role" parameter
-        # role = kwargs.get('role', 'owner')  # Default to "owner" if not provided
-
-        # # Create user-organisation association
-        # user_org = UserOrganisation.objects.create(user=new_user,
-        #                                            organisation=organisation,
-        #                                            role=role)
 
         return new_user  # return instance
 
@@ -39,15 +22,9 @@ class MyUserModelManager(models.Manager):
         """
         Hash password before user creation
         """
-        if not plain_pwd or not isinstance(plain_pwd, str) or plain_pwd.strip() == '':
-            raise ValueError("password must be a string")
         try:
-            salt = bcrypt.gensalt()
-            encoded_pwd = plain_pwd.encode()
-
-            hashed_pwd = bcrypt.hashpw(encoded_pwd, salt=salt)
-
-            return hashed_pwd
+            hashed_pwd = bcrypt.hashpw(plain_pwd.encode(), bcrypt.gensalt())
+            return hashed_pwd.decode()
         except Exception as exc:
             print(f'error hashing password: {exc}')
             raise Exception(
