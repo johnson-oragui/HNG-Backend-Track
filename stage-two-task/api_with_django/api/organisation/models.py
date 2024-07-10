11e2a77e9d726d6147468e7f205b873f15146cfe
johnson-oragui <johnson.oragui@gmail.com>
2024-07-10 09:40:32 +0100

    corrected a typo at the field created_at and updated_at

--- stage-two-task/api_with_django/api/organisation/models.py
+++ stage-two-task/api_with_django/api/organisation/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.timezone import now
 from api.utils import gen_uuid_str
+from datetime import datetime
 
 
 class Organisation(models.Model):
@@ -8,8 +9,8 @@ class Organisation(models.Model):
     name = models.CharField(null=False, max_length=100)
     description = models.TextField(max_length=1000)
     owner_email = models.EmailField(null=False, blank=False, max_length=60)
-    create_at = models.DateField(default=now)
-    update_at = models.DateField(default=now)
+    created_at = models.DateField(default=datetime.now)
+    updated_at = models.DateField(default=datetime.now)
 
     class Meta:
         db_table = "organisations"
