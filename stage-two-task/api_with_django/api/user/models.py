da3e5a241a6a1eaf935b5e914e36968157d6736d
johnson-oragui <johnson.oragui@gmail.com>
2024-07-10 09:39:27 +0100

    corrected a typo at the field created_at and updated_at, removed __str__ method

--- stage-two-task/api_with_django/api/user/models.py
+++ stage-two-task/api_with_django/api/user/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils.timezone import now
 from api.models import MyUserModelManager
 from api.utils import gen_uuid_str
+from datetime import datetime
 
 
 class User(models.Model):
@@ -15,20 +16,11 @@ class User(models.Model):
     email = models.EmailField(null=False, unique=True, max_length=60)
     password = models.CharField(null=False, max_length=80)
     phone = models.CharField(max_length=15)
-    create_at = models.DateField(default=now)
-    update_at = models.DateField(default=now)
+    created_at = models.DateField(default=datetime.now)
+    updated_at = models.DateField(default=datetime.now)
 
     class Meta:
         """
-
+        Defines meta dta for users table
         """
         db_table = "users"
-
-    def __str__(self):
-        """
-        String representation of user
-        """
-        one = f"id: {self.id}, firstName: {self.firstName}, lastName: {self.lastName}, "
-        two = f"email: {self.email}, password: {self.password}, phone: {self.phone}, "
-        three = f"created_at: {self.create_at}, updated_at: {self.update_at}"
-        return ("{" + one + '\n' + two + '\n' + three + "}")
