2eebb33b387f67944cffa740879ca35666fbebc1
johnson-oragui <johnson.oragui@gmail.com>
2024-07-11 10:10:45 +0100

    included urls for user, authentication, user_organisation, and swagger

--- stage-two-task/api_with_django/api/api/urls.py
+++ stage-two-task/api_with_django/api/api/urls.py
@@ -14,18 +14,29 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from authentication.views import register_user, login_user
-from user.views import get_user
-from user_organisation.views import get_organisations, get_organisation, add_user
+from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="User Organisation API",
+      default_version='v1',
+      description="API documentation for User Organisation",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@userorganisation.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/auth/register', register_user, name='register_user'),
-    path('api/auth/login', login_user, name='login_user'),
-    path('api/users/<str:id>', get_user, name="get_user"),
-    path('api/organisations', get_organisations, name="get_organisations"),
-    path('api/organisations/<str:orgId>', get_organisation, name="get_organisation"),
-    path('api/organisations/<str:orgId>/users', add_user, name='add_user'),
+    path('authentication/', include('authentication.urls')),
+    path('user/', include('user.urls')),
+    path('user_organisation/', include('user_organisation.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
