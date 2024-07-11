b997a761c4ab01162cce7e8d9be7034be9d5ca89
johnson-oragui <johnson.oragui@gmail.com>
2024-07-11 10:09:27 +0100

    used swagger  for documentation

--- stage-two-task/api_with_django/api/authentication/views.py
+++ stage-two-task/api_with_django/api/authentication/views.py
@@ -3,9 +3,27 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from authentication.utils import (validate_data, create_user,
                                   generate_token, check_user)
-from user.models import User
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+
+@swagger_auto_schema(
+    method='post',
+    operation_description="Register a new user",
+    responses={200: openapi.Response('Success', openapi.Schema(
+        type=openapi.TYPE_OBJECT,
+        properties={
+            'status': openapi.Schema(type=openapi.TYPE_STRING),
+            'message': openapi.Schema(type=openapi.TYPE_STRING),
+            'data': openapi.Schema(type=openapi.TYPE_OBJECT)
+        }
+    ))}
+)
+
 
 @csrf_exempt
+@api_view(['POST'])
 def register_user(request):
     """
     API endpoint to Register a user
@@ -42,7 +60,21 @@ def register_user(request):
         print(f'could not register user: {exc}')
         return JsonResponse(payload, status=400)
 
+@swagger_auto_schema(
+    method='post',
+    operation_description="Authenticates a user",
+    responses={200: openapi.Response('Success', openapi.Schema(
+        type=openapi.TYPE_OBJECT,
+        properties={
+            'status': openapi.Schema(type=openapi.TYPE_STRING),
+            'message': openapi.Schema(type=openapi.TYPE_STRING),
+            'data': openapi.Schema(type=openapi.TYPE_OBJECT)
+        }
+    ))}
+)
+
 @csrf_exempt
+@api_view(['POST'])
 def login_user(request):
     """
     Authenticates a user
